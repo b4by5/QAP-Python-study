093392e49c508e8a9803f689ded88bcb0cde23dc
b4by5 <pocatellochief@gmail.com>
2022-07-24 22:47:08 +0400

    Update 12.7.3.py
    Delete print(per_cent.get())

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,5 +1,4 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-#print(per_cent.get())
 money = float(input('Введите сумму вклада: '))
 deposit = [int(money * per_cent['ТКБ']/100),
            int(money * per_cent['СКБ']/100),
