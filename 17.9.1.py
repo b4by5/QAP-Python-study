532668817690b1cfd5cdec43109bee14680e4456
b4by5 <pocatellochief@gmail.com>
2022-08-29 16:05:02 +0400

    removed unnecessary variables and confusing naming

--- 17.9.1.py
+++ 17.9.1.py
@@ -2,9 +2,19 @@ sequence = '9 5 3 7 4 8 1 2 23 31 11'
 _list = list(map(int, sequence.split()))
 
 try:
-    num = int(input('Введите любое число: '))
+    number = int(input('Введите любое число: '))
 except ValueError:
     raise Exception('Условие ввода данных не соблюдено!')
+
+
+def bubble():
+    global _list
+    for i in range(len(_list)):
+        for j in range(len(_list) - i - 1):
+            if _list[j] > _list[j + 1]:
+                _list[j], _list[j + 1] = _list[j + 1], _list[j]
+    print(_list)
+    return _list
 
 
 def binary_search(array, num, left, right):
@@ -24,17 +34,5 @@ def binary_search(array, num, left, right):
         raise Exception('Введенное число выходит за пределы последовательности')
 
 
-def bubble():
-    global _list
-    for i in range(len(_list)):
-        for j in range(len(_list) - i - 1):
-            if _list[j] > _list[j + 1]:
-                _list[j], _list[j + 1] = _list[j + 1], _list[j]
-    print(_list)
-    return _list
-
-
-array = _list
-
 bubble()
-print(binary_search(array, num, 0, len(_list)))
+print(binary_search(_list, number, 0, len(_list)))
